9de85e4a128eb59fe60b7fa614e5a0ae13800345
Maksim-zip <yy.maksimromanov@yandex.ru>
2023-06-06 18:10:08 +0300

    Сделал 7 лабу

--- cmd/blog/main.go
+++ cmd/blog/main.go
@@ -1,25 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql" //Расширение для работы с БД
+	"github.com/jmoiron/sqlx"          //Расширение для использования select
 )
 
 const (
-	port = ":3000"
+	port         = ":3000"
+	dbDriverName = "mysql"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/home", index)
+	db, err := openDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbx := sqlx.NewDb(db, dbDriverName) //Распширяем функционал БД внутри GO
+
+	mux := http.NewServeMux() //Распределяем функции по урлам
+	mux.HandleFunc("/home", index(dbx))
 	mux.HandleFunc("/post", post)
 
 	// Реализуем отдачу статики
 	mux.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
 
 	log.Println("Start server " + port)
-	err := http.ListenAndServe(port, mux)
+	err = http.ListenAndServe(port, mux)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+// Соединяемся с БД
+func openDB() (*sql.DB, error) {
+	return sql.Open(dbDriverName, "root:1234567890@tcp(localhost:3306)/blog?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=true")
 }
---
+++ data/posts.sql
@@ -0,0 +1,155 @@
+INSERT INTO post (
+        title, 
+        subtitle, 
+        author, 
+        author_url, 
+        publish_date, 
+        image_url, 
+        featured,
+        modifier
+) 
+VALUES (
+    'The Road Ahead', 
+    'The road ahead might be paved — it might not be.', 
+    'Mat Vogels', 
+    '/assets/images/Mat_Vogels.png',
+    'September 25, 2015', 
+    '/assets/images/primary-article-cover1.jpg', 
+    1,
+    'primary-article_first'
+);
+
+INSERT INTO post (
+        title, 
+        subtitle, 
+        author, 
+        author_url, 
+        publish_date, 
+        image_url, 
+        featured,
+        modifier
+) 
+VALUES (
+    'From Top Down', 
+    'Once a year, go someplace you’ve never been before.', 
+    'William Wong', 
+    '/assets/images/William_Wong.png',
+    'September 25, 2015', 
+    '/assets/images/primary-article-cover2.jpg', 
+    1,
+    'primary-article_second'
+);
+
+INSERT INTO post (
+    title, 
+    subtitle, 
+    author, 
+    author_url, 
+    publish_date, 
+    image_url, 
+    featured
+) 
+VALUES (
+    'Still Standing Tall', 
+    'Life begins at the end of your comfort zone.', 
+    'William Wong', 
+    '/assets/images/William_Wong.png',
+    '9/25/2015', 
+    '/assets/images/article-cover1.jpg', 
+    0
+);
+
+INSERT INTO post (
+    title, 
+    subtitle, 
+    author, 
+    author_url, 
+    publish_date, 
+    image_url, 
+    featured
+) 
+VALUES (
+    'Sunny Side Up', 
+    'No place is ever as bad as they tell you it''s going to be.', 
+    'Mat Vogels', 
+    '/assets/images/Mat_Vogels.png',
+    '9/25/2015', 
+    '/assets/images/article-cover2.png', 
+    0
+);
+
+INSERT INTO post (
+    title, 
+    subtitle, 
+    author, 
+    author_url, 
+    publish_date, 
+    image_url, 
+    featured
+)
+VALUES (
+    'Water Falls', 
+    'We travel not to escape life, but for life not to escape us.', 
+    'Mat Vogels', 
+    '/assets/images/Mat_Vogels.png',
+    '9/25/2015', 
+    '/assets/images/article-cover3.png', 
+    0
+);
+
+INSERT INTO post (
+    title, 
+    subtitle, 
+    author, 
+    author_url, 
+    publish_date, 
+    image_url, 
+    featured
+) 
+VALUES (
+    'Through the Mist', 
+    'Travel makes you see what a tiny place you occupy in the world.', 
+    'William Wong', 
+    '/assets/images/William_Wong.png',
+    '9/25/2015', 
+    '/assets/images/article-cover4.png', 
+    0
+);
+
+INSERT INTO post (
+    title,
+    subtitle, 
+    author, 
+    author_url, 
+    publish_date, 
+    image_url, 
+    featured
+) 
+VALUES (
+    'Awaken Early', 
+    'Not all those who wander are lost.', 
+    'Mat Vogels', 
+    '/assets/images/Mat_Vogels.png',
+    '9/25/2015', 
+    '/assets/images/article-cover5.png', 
+    0
+);
+
+INSERT INTO post (
+    title, 
+    subtitle, 
+    author, 
+    author_url, 
+    publish_date, 
+    image_url, 
+    featured
+) 
+VALUES (
+    'Try It Always', 
+    'The world is a book, and those who do not travel read only one page.', 
+    'Mat Vogels', 
+    '/assets/images/Mat_Vogels.png',
+    '9/25/2015', 
+    '/assets/images/article-cover6.png', 
+    0
+    );
